67ad98ff2743dc029b04772f8827c1f03bb30e25
Vitor Scrittore <89667632+VitorSCRITTORE@users.noreply.github.com>
2024-09-05 19:44:57 -0300

    criado dados.js

---
+++ projeto/dados.js
@@ -0,0 +1,20 @@
+let dados = [
+
+{
+    titulo: "Rayssa Leal",
+    descricao: 'Rayssa Leal, conhecida como "Fadinha", é uma skatista profissional brasileira que se tornou a mais jovem medalhista olímpica da história do Brasil. Com seu talento e carisma, ela conquistou o mundo do skate e inspirou uma nova geração de skatistas. Rayssa é um exemplo de superação e determinação, mostrando que não há limites para os sonhos.',
+    link: "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+},
+
+{
+    titulo: "Ayrton Senna",
+    descricao: 'Ayrton Senna da Silva foi um piloto de Fórmula 1 brasileiro, considerado um dos maiores de todos os tempos. Conhecido por sua habilidade, determinação e paixão pelas corridas, conquistou três títulos mundiais de Fórmula 1. Sua morte trágica em 1994 o transformou em um ícone mundial do esporte, e sua história continua inspirando milhões de pessoas.',
+    link: "https://pt.wikipedia.org/wiki/Ayrton_Senna"
+},
+
+{
+    titulo: "Rebeca Andrade (Judo)",
+    descricao: 'Rebeca Andrade é uma judoca brasileira que se destaca pela sua técnica e força. Ela conquistou diversas medalhas em competições nacionais e internacionais, sendo um dos principais nomes do judô brasileiro. Rebeca é um exemplo de atleta dedicada e inspiradora.',
+    link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+}
+];
