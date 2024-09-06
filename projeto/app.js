96dcf5d3ba9fb2b263fb81a39aa6aeff184738f6
Vitor Scrittore <89667632+VitorSCRITTORE@users.noreply.github.com>
2024-09-05 22:59:50 -0300

    app.js

--- projeto/app.js
+++ projeto/app.js
@@ -1,1 +1,21 @@
-console.log(dados[0].link);
+console.log(dados);
+
+let section = document.getElementById("resultados-pesquisa");
+console.log(section);
+
+function pesquisar(){
+    let resultados = '';
+    for (let dado of dados){
+        resultados += `
+        <div class="item-resultado">
+                    <h2>
+                        <a href="#" target="_blank">${dado.titulo}</a>
+                    </h2>
+                    <p class="descricao-meta">${dado.descricao}</p>
+                    <a href=${dado.link} target="_blank">Saiba mais</a>
+                </div>`
+    }
+    section.innerHTML = resultados
+};
+
+
