b0fa5b288e1044898343d980b5963c6c7352bb29
Vitor Scrittore <89667632+VitorSCRITTORE@users.noreply.github.com>
2024-09-07 07:04:50 -0300

    adicionado pesquisa e tags

--- projeto/app.js
+++ projeto/app.js
@@ -1,21 +1,63 @@
-console.log(dados);
+// Exibe o conteúdo da variável 'dados' no console do navegador.
+// Essa variável provavelmente contém um array de objetos com informações sobre os resultados da pesquisa.
+//console.log(dados);
 
+// Obtém a referência ao elemento HTML com o ID "resultados-pesquisa".
+// Este elemento será utilizado para exibir os resultados da pesquisa.
 let section = document.getElementById("resultados-pesquisa");
-console.log(section);
+//console.log(section);
 
+// Camel case: convenção de nomenclatura em que nomes de variáveis iniciam com letra minúscula e palavras subsequentes iniciam com letra maiúscula.
+// Definir variável com nome duplo: neste caso, "campoPesquisa" é mais claro do que "campo_pesquisa".
+
+// Função que realiza a pesquisa e atualiza a seção de resultados.
 function pesquisar(){
-    let resultados = '';
+    // Obtém novamente a referência à seção de resultados (poderia ser armazenada fora da função para otimização).
+    let section = document.getElementById("resultados-pesquisa")
+
+    // Obtém a referência ao elemento HTML com o ID "campo-pesquisa".
+    // Este elemento é um campo de input onde o usuário digita a pesquisa.
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
+
+    // Ignora se entrada for vazia
+    if (campoPesquisa == ""){
+        section.innerHTML = "<p>Nada foi encontrado. Digite o nome do atleta ou esporte</p>"
+        return
+    }
+    // Converte
+    campoPesquisa = campoPesquisa.toLowerCase();
+
+    // Inicializa uma string vazia para armazenar os resultados da pesquisa formatados em HTML.
+    let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
+
+    // Itera sobre cada elemento (dado) do array 'dados'.
     for (let dado of dados){
-        resultados += `
-        <div class="item-resultado">
+        //console.log(dado.titulo)
+        //console.log(campoPesquisa)
+        titulo = dado.titulo.toLowerCase();
+        descricao = dado.descricao.toLocaleLowerCase();
+        tags = dado.tags.toLowerCase();
+        // Verifica se o título contém a palavra pesquisada
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)){
+            resultados += `
+                <div class="item-resultado">
                     <h2>
                         <a href="#" target="_blank">${dado.titulo}</a>
                     </h2>
                     <p class="descricao-meta">${dado.descricao}</p>
                     <a href=${dado.link} target="_blank">Saiba mais</a>
+                    <p>tags: ${tags}</p>
                 </div>`
+        };
+    };
+
+    if (!resultados){
+        resultados = "<p>Nada foi encontrado</p>"
     }
-    section.innerHTML = resultados
+    // Atribui o conteúdo HTML gerado à propriedade 'innerHTML' da seção de resultados,
+    // substituindo o conteúdo anterior.
+    section.innerHTML = resultados;
 };
-
-
--- projeto/dados.js
+++ projeto/dados.js
@@ -2,18 +2,21 @@ let dados = [
 {
     titulo: "Rayssa Leal",
     descricao: 'Rayssa Leal, conhecida como "Fadinha", é uma skatista profissional brasileira que se tornou a mais jovem medalhista olímpica da história do Brasil. Com seu talento e carisma, ela conquistou o mundo do skate e inspirou uma nova geração de skatistas. Rayssa é um exemplo de superação e determinação, mostrando que não há limites para os sonhos.',
-    link: "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+    link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+    tags: "skate fada sk8"
 },
 
 {
     titulo: "Ayrton Senna",
     descricao: 'Ayrton Senna da Silva foi um piloto de Fórmula 1 brasileiro, considerado um dos maiores de todos os tempos. Conhecido por sua habilidade, determinação e paixão pelas corridas, conquistou três títulos mundiais de Fórmula 1. Sua morte trágica em 1994 o transformou em um ícone mundial do esporte, e sua história continua inspirando milhões de pessoas.',
-    link: "https://pt.wikipedia.org/wiki/Ayrton_Senna"
+    link: "https://pt.wikipedia.org/wiki/Ayrton_Senna",
+    tags: "piloto formula1 campeao"
 },
 
 {
     titulo: "Rebeca Andrade",
     descricao: 'Rebeca Andrade é uma judoca brasileira que se destaca pela sua técnica e força. Ela conquistou diversas medalhas em competições nacionais e internacionais, sendo um dos principais nomes do judô brasileiro. Rebeca é um exemplo de atleta dedicada e inspiradora.',
-    link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+    link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+    tags: "ginástica ouro"
 }
 ];
